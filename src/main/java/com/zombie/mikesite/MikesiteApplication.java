0001b91daabb8cb6df7e015c790b95bd36c2a2c0
Michael Fouche <mfouche@impactradius.com>
2018-10-20 07:46:39 +0200

    Initial push

---
+++ src/main/java/com/zombie/mikesite/MikesiteApplication.java
@@ -0,0 +1,13 @@
+package com.zombie.mikesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikesiteApplication.class, args);
+		System.out.println("Hello");
+	}
+}
