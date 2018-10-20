33d0673b3ccc156dd84e8068a26fe43a11d973ff
Michael Fouche <mfouche@impactradius.com>
2018-10-20 08:23:36 +0200

    add failing unit test

--- src/test/java/com/zombie/mikesite/MikesiteApplicationTests.java
+++ src/test/java/com/zombie/mikesite/MikesiteApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zombie.mikesite;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,11 @@ public class MikesiteApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testTrue() {
+		Assert.assertTrue(false);
 	}
 
 }
